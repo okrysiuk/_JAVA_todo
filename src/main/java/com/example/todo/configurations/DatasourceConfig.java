6c50e4540ba916b32fedb481da60dab70f037be7
Oleksandr Krysiuk <oleksandr.krysiuk@gmail.com>
2020-08-25 15:08:24 +0300

    ..

--- src/main/java/com/example/todo/configurations/DatasourceConfig.java
+++ src/main/java/com/example/todo/configurations/DatasourceConfig.java
@@ -1,4 +1,4 @@
-package com.example.personalblog.configurations;
+package com.example.todo.configurations;
 
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +12,7 @@ public class DatasourceConfig {
     public DataSource datasource() {
         return DataSourceBuilder.create()
                 .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url("jdbc:mysql://127.0.0.1:3306/pbdb?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC")
+                .url("jdbc:mysql://127.0.0.1:3306/todo?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC")
                 .username("tomcat")
                 .password("rootR0ot")
                 .build();
